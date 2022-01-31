555147c0737591e14b527f55c3104b2b84c1562d
alenakulyakhtina <91906350+alenakulyakhtina@users.noreply.github.com>
2022-01-31 15:59:38 +0300

    Update Lab3.1.c

--- Lab3.1.c
+++ Lab3.1.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 
 int main() {
+    // PID - запущенный процесс
+    // PPID - номер процесса, от которого он запущен.
     printf ("User PID: %d\n", getpid());
     printf ("User PPID: %d\n\n", getppid());
     return 0;
