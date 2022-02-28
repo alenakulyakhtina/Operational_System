955c3ccb3118ad9e3bbac4af3fe1d99f922a7ab4
alenakulyakhtina <91906350+alenakulyakhtina@users.noreply.github.com>
2022-02-28 15:56:36 +0300

    Lab 7.2 (done)

---
+++ Lab7.2.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+// 2
+int main()
+{
+  char    *array;   
+  int     shmid;     
+  int     new = 0;   
+  char    pathname[] = "07-1a.c"; 
+                                  
+  key_t   key;
+
+  if ((key = ftok(pathname,0)) < 0) {
+    printf("Can\'t generate key\n");
+    exit(-1);
+  }
+
+  if ((shmid = shmget(key, 5000*sizeof(char), 0666|IPC_CREAT)) < 0) { 
+      printf("Can\'t find shared memory\n");
+      exit(-1);
+    
+  }
+
+  if ((array = (char *)shmat(shmid, NULL, 0)) == (char *)(-1)) {
+    printf("Can't attach shared memory\n");
+    exit(-1);
+  }
+  // Печатаем пока не закончатся строки.
+  while(array[new] != '\0') {
+    printf("%c", array[new]);
+    new++;
+  }
+  printf("OK");
+  if (shmdt(array) < 0) {
+    printf("Can't detach shared memory\n");
+    exit(-1);
+  }
+
+  return 0;
+}
