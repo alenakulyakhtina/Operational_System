f323f51aa30189ec8d7504e80a1af15aa86f5932
alenakulyakhtina <91906350+alenakulyakhtina@users.noreply.github.com>
2022-05-30 14:56:51 +0300

    Lab 14_2 (done)

--- Lab14_2.c
+++ Lab14_2.c
@@ -7,17 +7,17 @@
 #include <signal.h>
 
 int main(void) {
-  (void)signal(SIGINT, isSignal);
-  (void)signal(SIGQUIT, isSignal);
+  (void)signal(SIGINT, my_handler);
+  (void)signal(SIGQUIT, my_handler);
 
   while(1);
 
   return 0;
 }
 
-void isSignal(int signl) {
-  if (signl == 2)
-    printf("It's CTRL-C: %d\n", signl);
-  else if (signl == 3)
-    printf("It's CTRL-4: %d\n", signl);
+void my_handler(int nsig) {
+  if (nsig == 2)
+    printf("It's CTRL-C: %d\n", nsig);
+  else if (nsig == 3)
+    printf("It's CTRL-4: %d\n", nsig);
 }
